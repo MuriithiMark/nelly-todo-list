80cce482eca3a68364e8427767a4de53160f2af1
MuriithiMark <markmwangi14317@gmail.com>
2024-01-24 17:50:26 +0300

    todo-list

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,50 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import TodoAdder from "./components/TodoAdder";
+import TodoList from "./components/TodoList";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [todos, setTodos] = useState([
+    {
+      id: 1,
+      title: "Nelly is Visiting",
+      completed: false,
+      deleted: false,
+    },
+    {
+      id: 2,
+      title: "Washing dishes",
+      completed: false,
+      deleted: false,
+    },
+  ]);
 
+  function handleAdd(inputText) {
+    console.log(inputText);
+    let newId = 1;
+    if(todos.length > 0) {
+      newId = todos.length + 1;
+    }
+    const newTodo = {
+      id: newId,
+      title: inputText,
+      completed: false,
+      deleted: false,
+    };
+    setTodos([...todos, newTodo])
+  }
+
+  function handleDelete(id){
+    console.log(id)
+    const todosWithoutDeleted = todos.filter((todo) => todo.id !== id);
+    setTodos(todosWithoutDeleted);
+  }
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div className="app">
+      <h1>To do list</h1>
+      <TodoAdder onAdd={handleAdd} />
+      <TodoList todos={todos}  onDelete={handleDelete}/>
+    </div>
+  );
 }
 
-export default App
+export default App;
